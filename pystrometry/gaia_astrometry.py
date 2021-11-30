89e7dbd02d56b01993c6b7671d2247b872b2c7da
Johannes Sahlmann <jsahlmann@sciops.esa.int>
2021-11-30 21:16:40 +0100

    bug fix

--- pystrometry/gaia_astrometry.py
+++ pystrometry/gaia_astrometry.py
@@ -18,7 +18,8 @@ from astropy.time import Time
 import astropy.units as u
 import numpy as np
 
-from .pystrometry import OrbitSystem, convert_from_angular_to_linear, pjGet_m2, AstrometricOrbitPlotter, MS_kg, MJ_kg
+from .pystrometry import OrbitSystem, convert_from_angular_to_linear, pjGet_m2, \
+    AstrometricOrbitPlotter, MS_kg, MJ_kg, AstrometricPpmPlotter
 
 def robust_scatter_estimate(data, verbose=True):
     """Return the Robust Scatter Estimate (RSE) es defined for Gaia.
@@ -649,7 +650,7 @@ def plot_individual_ppm(parameter_dict, iad, plot_dir=os.path.expanduser('~')):
     else:
         mag_str = ''
 
-        
+    source_id = parameter_dict['sourceId']    
     axp.title = 'Gaia DR4 {} ({}, {})'.format(source_id, 'LPC', mag_str)
     name_seed = 'DR4_{}'.format(source_id)
 
