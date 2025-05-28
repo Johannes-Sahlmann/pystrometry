20fd998ec79aea5c41ee7c6a59b6e8490ff89a5a
Johannes Sahlmann <Johannes.Sahlmann@esa.int>
2025-05-28 22:55:00 +0200

    modify test

--- pystrometry/tests/test_get_ephemeris.py
+++ pystrometry/tests/test_get_ephemeris.py
@@ -1,6 +1,10 @@
+import os
+
 from astropy.table import Table
 from astropy.time import Time
+
 from ..pystrometry import get_ephemeris
+from ..pystrometry import ephemeris_dir
 
 def test_get_ephemeris_valid_input():
     # Define valid test inputs
@@ -9,6 +13,11 @@ def test_get_ephemeris_valid_input():
     start_time = Time('2023-01-01T00:00:00')
     stop_time = Time('2024-01-10T00:00:00')
     step_size = '1d'
+
+    print(f"\n{ephemeris_dir}\n")
+    horizons_file_seed = '{}_{}_{}_{}_{}'.format(center, target, start_time, stop_time, step_size)
+    out_file = os.path.join(ephemeris_dir, horizons_file_seed + '.txt')
+    assert os.path.isfile(out_file), f"Expected output file {out_file} does not exist."
 
     # Call the function
     result = get_ephemeris(center=center, target=target, start_time=start_time, stop_time=stop_time, step_size=step_size)
